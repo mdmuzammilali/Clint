e5675b48c74113bd2d3972f76d2914e1ffb2c511
Sharjeel <sharjeelhussain877@gmail.com>
2025-11-17 22:14:34 +0500

    enable search base on collection route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, useLocation } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 
 
 const Navbar = () => {
+    const location = useLocation();
     const [visible, setVisible] = useState(false);
-     
-    const{setShowSearch, getCartCount} = useContext(ShopContext);
+
+    const isCollectionRoute = location.pathname.includes('collection')
+
+    const { setShowSearch, getCartCount } = useContext(ShopContext);
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
@@ -35,9 +38,12 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+                {
+                    isCollectionRoute &&
+                    <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+                }
                 <div className='group relative'>
-                   <Link to='/login'> <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" /> </Link> 
+                    <Link to='/login'> <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" /> </Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 '>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>MyProfile</p>
